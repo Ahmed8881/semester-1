3e3ff4409b3dd912c0bb48eff3602d36cf1cf50b
M Ahmed Butt <144407474+Ahmed8881@users.noreply.github.com>
2024-01-02 23:28:23 +0500

    Add files via upload

---
+++ PD/week 8/modinverse.cpp
@@ -0,0 +1,23 @@
+int modInverse(int a, int m) {
+    int m0 = m;
+    int y = 0, x = 1;
+
+    if (m == 1)
+        return 0;
+
+    while (a > 1) {
+        int q = a / m;
+        int t = m;
+
+        m = a % m, a = t;
+        t = y;
+
+        y = x - q * y;
+        x = t;
+    }
+
+    if (x < 0)
+        x += m0;
+
+    return x;
+}
