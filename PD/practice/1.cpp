1e6515f3362c706890670daa45f7b6614de8bda7
M Ahmed Butt <144407474+Ahmed8881@users.noreply.github.com>
2024-03-09 12:10:27 +0500

    Update 1.cpp

--- PD/practice/1.cpp
+++ PD/practice/1.cpp
@@ -1,1 +1,22 @@
+#include<iostream>
+using namespace std;
+main(){
+ 
+  
+int cars[5][5] = {{10, 7, 12, 10, 4},
+                  {18, 11, 15, 17, 2},
+                  {23, 19, 12, 16, 14},
+                  {7, 12, 16, 0, 2},
+                  {3, 5, 6, 2, 1}};
+int largest=0;
+for(int i=0;i<5;i++){
+    for(int j=0;j<5;j++){
+        if(cars[i][j]>15){
+            largest++;
+        }
+    }
+}
+cout<<largest;
+}
+ 
 
